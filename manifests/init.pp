ba3660754bdda02d0b0ec52b9ab16a017ae31889
@slovacus <slovacus@gmail.com>
2015-07-05 13:13:18 -0500

    [feature/#1] values variables to class phantomjs

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,7 +8,9 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class phantomjs (
-  $package_dependences = $::phantomjs::params::package_dependences
+  $package_develop = $::phantomjs::params::package_develop,
+  $app_name = $::phantomjs::params::app_name,
+  $version_repository = $::phantomjs::params::version_repository,
 ) inherits ::phantomjs::params {
 
   # validate parameters here
