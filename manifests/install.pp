bd7101a308669f0274ad39a3cb80e0e6c0e045a3
@slovacus <slovacus@gmail.com>
2015-07-05 21:09:06 -0500

    [feature/#1] fixed install module phantomjs

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,33 +4,8 @@
 #
 class phantomjs::install {
 
-  package {
-    $::phantomjs::package_develop:
-    ensure => present,
-  }
-
-  exec { "phantomjs clone repository":
-    command => "git clone git://github.com/ariya/phantomjs.git",
-    cwd     => "/var/tmp/",
-    require => Package["git"],
-    unless  => "ls -l /var/tmp/$::phantomjs::app_name",
-  }
-
-  exec { "phantomjs version repository":
-    command => "git checkout $::phantomjs::version_repository",
-    cwd     => "/var/tmp/$::phantomjs::app_name",
-    unless  => "git branch | grep $::phantomjs::version_repository",
-    require => [Exec["phantomjs clone repository"]],
-  }
-
-  exec { "phantomjs build":
-    command => "/var/tmp/$::phantomjs::app_name/build.sh --confirm",
-    returns => [0, 2, 14],
-    timeout => 3600,
-    cwd     => "/var/tmp/$::phantomjs::app_name",
-    require => [Exec["phantomjs version repository"]],
-    unless  => "which $::phantomjs::app_name",
-    logoutput  => true,
+  package { $::phantomjs::package:
+    ensure => installed,
   }
 
 }
